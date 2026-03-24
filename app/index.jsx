becdbbb8a16497b99b11fd5e2c29649fa96dbd7a
Tamas <tamasgavlider@gmail.com>
2026-03-24 14:08:09 +0100

    add button

---
+++ app/index.jsx
@@ -0,0 +1,72 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+import React from "react";
+import bgImage from "@/assets/images/bg-ai.jpg";
+import { Link } from "expo-router";
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={bgImage} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>CivilAI</Text>
+        <Link href="/explore" style={{ marginHorizontal: "auto" }} asChild>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Explore</Text>
+          </Pressable>
+        </Link>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default app;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    marginBottom: 120,
+  },
+  link: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    textDecorationLine: "underline",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    padding: 4,
+  },
+  button: {
+    height: 60,
+    borderRadius: 20,
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.75)",
+    padding: 6,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 4,
+  },
+});
