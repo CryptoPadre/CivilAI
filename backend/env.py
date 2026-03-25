c6c655db7cc78bf781e1f7dec876d2761c150e5d
Tamas <tamasgavlider@gmail.com>
2026-03-25 14:04:39 +0100

    connect DB for api

---
+++ backend/env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault('DATABASE_URL', 'postgresql://neondb_owner:npg_DCmdoQ8PO0pL@ep-crimson-resonance-amjq27sj-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
+os.environ.setdefault('SECRET_KEY', 'django-insecure-^ffwlq5or!&$e*-+i30#w)+m9nnk=f^lx^=51@74qo*%@(1vcf')
