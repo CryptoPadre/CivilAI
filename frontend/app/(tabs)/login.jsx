21fff69e3a44b5f126c0fe84908070c28c2aaac3
Tamas <tamasgavlider@gmail.com>
2026-03-31 14:50:50 +0200

    validate form

--- frontend/app/(tabs)/login.jsx
+++ frontend/app/(tabs)/login.jsx
@@ -1,6 +1,114 @@
 import React from "react";
-import { Text } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { useState } from "react";
 
-export default function SignInScreen() {
-  return <Text>What is it?</Text>;
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    let errors = {};
+
+    if (!username) errors.username = "Username is required";
+    if (!password) errors.password = "Password is required";
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = () => {
+    if (validateForm()) {
+      console.log("Submitted", username, password);
+      setUsername("");
+      setPassword("");
+      setErrors({});
+    }
+  };
+  return (
+    <KeyboardAvoidingView
+      behavior="padding"
+      style={styles.container}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
+    >
+      <Image
+        source={require("@/assets/images/loginimg.png")}
+        style={styles.image}
+      ></Image>
+
+      {/* Username */}
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      {errors.username ? (
+        <Text style={styles.errorText}>{errors.username}</Text>
+      ) : null}
+
+      {/* Password */}
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      {errors.password ? (
+        <Text style={styles.errorText}>{errors.password}</Text>
+      ) : null}
+      {/* Button */}
+      <Button title="Login" onPress={handleSubmit} />
+      {/* Link */}
+      <TouchableOpacity>
+        <Text style={styles.link}>Don’t have an account? Sign up now!</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    padding: 12,
+    marginBottom: 10,
+    textAlign: "center",
+    backgroundColor: "green",
+    color: "white",
+  },
+  errorText: {
+    marginBottom: 10,
+    color: "red",
+    textAlign: "center",
+  },
+  link: {
+    marginTop: 20,
+    textAlign: "center",
+    color: "green",
+  },
+  image: {
+    width: 200,
+    height: 400,
+    alignSelf: "center",
+    marginBottom: 50,
+  },
+});
---
+++ frontend/assets/api/auth.jsx
@@ -0,0 +1,6 @@
+import { SafeAreaViewBase } from "react-native";
+import React, { Component } from "react";
+
+function auth() {}
+
+export default auth;
