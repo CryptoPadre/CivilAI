292f1acb273c5643e911a30d68404af71d753c14
Tamas <tamasgavlider@gmail.com>
2026-04-06 14:09:42 +0200

    add filter to npc list view

--- civilAI/npc/views.py
+++ civilAI/npc/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import django_filters.rest_framework
 from rest_framework import permissions, generics, filters
 from .serializers import NpcSerializer
 from .models import Npc
@@ -14,7 +15,12 @@ class  NpcListView(generics.ListCreateAPIView):
          'first_name',
          'last_name'
         ]
-
+    filter_backends = [
+        django_filters.rest_framework.DjangoFilterBackend,
+        filters.SearchFilter
+    ]
+    
+    
 class NpcDetailView(generics.RetrieveAPIView):
     queryset = Npc.objects.all()
     serializer_class = NpcSerializer
